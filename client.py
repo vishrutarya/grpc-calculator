1920079c2006d9d1f3643a84525bc33fa74cbd5d
Vishrut Arya <12104721+vishrutarya@users.noreply.github.com>
2020-01-21 12:32:00 -0800

    improved results printing

--- client.py
+++ client.py
@@ -2,6 +2,18 @@ import grpc
 
 import calculator_pb2, calculator_pb2_grpc
 
+def print_results(request, response, procedure_name) -> None:
+    """
+    Prints results of a fulfilled request.
+    """
+    procedure_names_dict = {
+        'SquareRoot': calculator_pb2_grpc.CalculatorServicer.SquareRoot.__name__,
+        'Square': calculator_pb2_grpc.CalculatorServicer.Square.__name__,
+    }
+    print_string = f"Request: {procedure_names_dict[procedure_name]} for {request.value}.\nResponse: {response.value}.\n"
+    print(print_string)
+
+
 # open a gRPC channel
 channel = grpc.insecure_channel("localhost:50051")
 
@@ -14,15 +26,16 @@ request = calculator_pb2.Number(value=9)
 # make the remote procedure call (rpc)
 response = stub.SquareRoot(request)
 
-print(f"Request: 'square root' of {request.value}. Response: {response.value}")
+print_results(request, response, 'SquareRoot')
 
-# make new request and fulfill
+
+# make new requests and fulfill
 request = calculator_pb2.Number(value=16)
 response = stub.SquareRoot(request)
-print(f"Request: 'square root' of {request.value}. Response: {response.value}")
+print_results(request, response, 'SquareRoot')
 
 
-# test: square()
+# request: square
 request = calculator_pb2.Number(value=5)
 response = stub.Square(request)
-print(f"Request 'square' of {request.value}. Response: {response.value}")
+print_results(request, response, 'Square')
